8aa97c2f009035010ec3f4562e58dbb70e579ab0
Vijay P V <pvvijayraj1998@gmail.com>
2021-12-19 11:37:53 +0530

    storing image in local

--- index.js
+++ index.js
@@ -1,81 +1,52 @@
-const express = require("express")
-const path = require("path")
-const multer = require("multer")
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
 const app = express()
-	
-// View Engine Setup
-app.set("views",path.join(__dirname,"views"))
-app.set("view engine","ejs")
-	
-// var upload = multer({ dest: "Upload_folder_name" })
-// If you do not want to use diskStorage then uncomment it
-	
-var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
 
-		// Uploads is the Upload_folder_name
-		cb(null, "uploads")
-	},
-	filename: function (req, file, cb) {
-	cb(null, file.fieldname + "-" + Date.now()+".jpg")
-	}
+// const upload = multer({
+//     dest: 'images/'
+// });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, 'tempFile' + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCbJ-PKG_pbWc1vdTMOz1sTzc9bFxHIrc",
+    authDomain: "fir-test-project-b145a.firebaseapp.com",
+    projectId: "fir-test-project-b145a",
+    storageBucket: "fir-test-project-b145a.appspot.com",
+    messagingSenderId: "861816435611",
+    appId: "1:861816435611:web:d12c1e30710b155b17e73e",
+    measurementId: "${config.measurementId}"
+  };
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        res.status(400).send("Error: No files found")
+    } else {
+        // fs.writeFile('images/'+req.file.filename, req.file.)
+        console.log("File saved to local !");
+
+        res.status(200).send("file uploaded !");
+    }
 })
-	
-// Define the maximum size for uploading
-// picture i.e. 1 MB. it is optional
-const maxSize = 1 * 1000 * 1000;
-	
-var upload = multer({
-	storage: storage,
-	limits: { fileSize: maxSize },
-	fileFilter: function (req, file, cb){
-	
-		// Set the filetypes, it is optional
-		var filetypes = /jpeg|jpg|png/;
-		var mimetype = filetypes.test(file.mimetype);
 
-		var extname = filetypes.test(path.extname(
-					file.originalname).toLowerCase());
-		
-		if (mimetype && extname) {
-			return cb(null, true);
-		}
-	
-		cb("Error: File upload only supports the "
-				+ "following filetypes - " + filetypes);
-	}
-
-// mypic is the name of file attribute
-}).single("mypic");	
-
-app.get("/",function(req,res){
-	res.render("Signup");
-})
-	
-app.post("/uploadProfilePicture",function (req, res, next) {
-		
-	// Error MiddleWare for multer file upload, so if any
-	// error occurs, the image would not be uploaded!
-	upload(req,res,function(err) {
-
-		if(err) {
-
-			// ERROR occured (here it can be occured due
-			// to uploading image of size greater than
-			// 1MB or uploading different file type)
-			res.send(err)
-		}
-		else {
-
-			// SUCCESS, image successfully uploaded
-			res.send("Success, Image uploaded!")
-		}
-	})
-})
-	
-// Take any port number of your choice which
-// is not taken by any other process
-app.listen(8080,function(error) {
-	if(error) throw error
-		console.log("Server created Successfully on PORT 8080")
+app.listen(8080, () => {
+    console.log('🚀Server listening on port 8080')
 })
